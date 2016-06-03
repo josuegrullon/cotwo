42ef27983ffd49a8fb12934dbbee42c3246586d4
josue <josuegrullon@gmail.com>
2016-06-03 09:55:43 -0400

    Add map

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,91 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<style type="text/css" media="screen">
+    #map_wrapper {
+    height: 400px;
+}
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+#map_canvas {
+    width: 100%;
+    height: 100%;
+}
+</style>
 
-        <style>
-            html, body {
-                height: 100%;
+<script>
+
+    $(function() {
+
+       
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "//maps.googleapis.com/maps/api/js?callback=initialize";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+    
+    var markers = [];
+    var infoWindowContent = [];
+    // Multiple Markers
+     $.ajax({
+            url: 'http://cotwo-api.com/v1/collectors/measurements',
+            method: 'GET',
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                $.each(data, function (index, value) {
+                    markers.push([value.name, value.latitude,value.longitude]);
+                    infoWindowContent.push([
+                        '<div class="info_content">' +
+                        '<h3>'+value.name+'</h3>' +
+                        '<p>Valor promedio: '+value.avg+' ppm</p>' +        '</div>'
+                    ]);
+                });
+                
             }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
+        });
+    
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
             }
+        })(marker, i));
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
+</script>
+<div id="map_wrapper">
+    <div id="map_canvas" class="mapping"></div>
+</div>
